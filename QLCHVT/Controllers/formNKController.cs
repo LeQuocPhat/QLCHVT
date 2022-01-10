5b0f6e88236aa53957108de3cbac17a3341658ea
LeQuocPhat <phatphatle9999@gmail.com>
2022-01-10 11:02:28 +0700

    hoàn thành đơn nhập hàng

--- QLCHVT/Controllers/formNKController.cs
+++ QLCHVT/Controllers/formNKController.cs
@@ -21,35 +21,103 @@ namespace QLCHVT.Controllers
 
         public ActionResult AddOrder()
         {
-            List<VatTu> vattu = db.VatTus.ToList();
-            List<Nhanvien> nhanvien = db.Nhanviens.ToList();
-
-            // Tạo SelectList
-            SelectList cateList = new SelectList(vattu, "MaVT", "TenVT");
-            SelectList nhanVien = new SelectList(nhanvien, "MaNV", "Ten");
-
-            // Set vào ViewBag
-            ViewBag.VatTu = cateList;
+            if (Session["giohang"] == null)//chuw alive tao de add vaof carrt
+            {
+                Session["giohang"] = new List<CartItem>();
+            }
+            
+            List<Nhanvien> NhanVien = db.Nhanviens.ToList();
+            //// Tạo SelectList
+            SelectList nhanVien = new SelectList(NhanVien, "MaNV", "Ten");
+            //// Set vào ViewBag
+            List<CartItem> giohang = Session["giohang"] as List<CartItem>;
             ViewBag.NhanVien = nhanVien;
+            ViewBag.GioHang = giohang;
             return View();
         }
 
-        [HttpPost]
-        public ActionResult AddOrder([Bind(Include = "MaNK,MaVT,Soluong,DonGiaNhap")] ChiTietNhapKho CTNK,
-            [Bind(Include = "MaNK,MaNV,NgayLapPhieu")] NhapKho NK, FormCollection form)
+        public RedirectToRouteResult AddToForm(string MaSP)
         {
+           
+            //update carrt
+            List<CartItem> giohang = Session["giohang"] as List<CartItem>;
+            //kiemtra gio hang co sp dax chonj ch, neu chua co add
+            if (giohang.FirstOrDefault(c => c.MaVT == MaSP) == null)//return câu select đầu tiên
+            {
+                VatTu sp = db.VatTus.Find(MaSP);
+                CartItem newCart = new CartItem();
+                newCart.MaVT = MaSP;
+                newCart.TenVT = sp.TenVT;
+                newCart.SoLuongt = 1;
+                newCart.DonGiaNhapt = Convert.ToDouble(sp.Dongia);
+                giohang.Add(newCart);
+            }
+            else //sp đã có trong cart và tăng sp
+            {
+                CartItem item = giohang.FirstOrDefault(c => c.MaVT == MaSP);//tìm phần tử cartItem trong gio hàng theo MaSp
+                item.SoLuongt++;
+            }
+            Session["giohang"] = giohang;
+            ViewBag.GioHang = giohang;
+            return RedirectToAction("AddOrder");
+
+        }
+
+        public ActionResult SelectST()
+        {
+
+            var vatTus = db.VatTus.Include(v => v.LoaiVatTu);
+            return View(vatTus.ToList());
+        }
+
+        public RedirectToRouteResult Delete(string MaSP)
+        {
+            List<CartItem> giohang = Session["giohang"] as List<CartItem>;
+            CartItem item = giohang.FirstOrDefault(c => c.MaVT == MaSP);
+            if (item != null)
+            {
+                giohang.Remove(item);
+                Session["giohang"] = giohang;
+            }
+            return RedirectToAction("AddOrder");
+        }
+
+        //public int Insert(ChiTietNhapKho ctnk)
+        //{
+        //    db.ChiTietNhapKhoes.Add(ctnk);
+        //    return db.SaveChanges();
+        //}
+
+        [HttpPost]
+        public ActionResult AddOrder(FormCollection form)
+        {
+            
+            NhapKho NK = new NhapKho();
+            List<CartItem> giohang = Session["giohang"] as List<CartItem>;
             if (ModelState.IsValid)
             {
-                var Mavalid = db.NhapKhoes.Where(x => x.MaNK == form["MaNK"]);
-                if(Mavalid == null)
-                {
+                
                     NK.MaNK = form["MaNK"];
-                    NK.MaNV = int.Parse(form["NhanVien"]);
+                    //NK.MaNV = int.Parse(form["NhanVien"]);
+                    NK.MaNV = int.Parse(form["NhanVien"]); 
+                    NK.NgayLapPhieu = DateTime.Parse(form["NgayLapPhieu"]) ;
                     db.NhapKhoes.Add(NK);
+                    db.SaveChanges();
+                
+                string mank = form["MaNK"];
+                
+                foreach (var item in giohang)
+                {
+                    ChiTietNhapKho CTNK = new ChiTietNhapKho();
+                    CTNK.MaNK = mank;
+                    CTNK.MaVT = item.MaVT;
+                    CTNK.Soluong = (short?)item.SoLuongt;
+                    CTNK.DongiaNhap = (float?)item.DonGiaNhapt;
+                    //this.Insert(CTNK);
+                    db.ChiTietNhapKhoes.Add(CTNK);
+                    db.SaveChanges();
                 }
-                CTNK.MaVT = form["TenVT"];
-                db.ChiTietNhapKhoes.Add(CTNK);
-                db.SaveChanges();
+                
                 return RedirectToAction("Index");
             }
             return View();
---
+++ QLCHVT/Models/CartItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QLCHVT.Models
+{
+    public class CartItem
+    {
+        public string MaVT { get; set; }
+        public string TenVT { get; set; }
+        public int SoLuongt { get; set; }
+        public double DonGiaNhapt { get; set; }
+        public double ThanhTien
+        {
+            get
+            {
+                return DonGiaNhapt * SoLuongt;
+            }
+        }
+
+    }
+}
